5049bacb06db3f7e6e50af7ec491a8dadd3321f2
Nikolay Dobychin <koddek@bk.ru>
2023-05-10 12:08:51 +0300

    Add custom fonts and add extensions for UIFont

---
+++ WaveNews/Extensions/UIFont + Extensions.swift
@@ -0,0 +1,38 @@
+//
+//  UIFont + Extensions.swift
+//  WaveNews
+//
+//  Created by KODDER on 10.05.2023.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    //Regular
+    static func interRegular14() -> UIFont? {
+        return UIFont(name: "Inter-Regular", size: 14)
+    }
+    
+    static func interRegular16() -> UIFont? {
+        return UIFont(name: "Inter-Regular", size: 16)
+    }
+    
+    //Bold
+    static func interBold16() -> UIFont? {
+        return UIFont(name: "Inter-Bold", size: 16)
+    }
+    
+    //Semi-Bold
+    static func interSemiBold12() -> UIFont? {
+        return UIFont(name: "Inter-SemiBold", size: 12)
+    }
+    
+    static func interSemiBold16() -> UIFont? {
+        return UIFont(name: "Inter-SemiBold", size: 16)
+    }
+    
+    static func interSemiBold24() -> UIFont? {
+        return UIFont(name: "Inter-SemiBold", size: 24)
+    }
+}
